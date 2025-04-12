573193f4532bfe2011ab7e7b97818185e18c8c47
ARUP JYOTI DUTTA <anraar1@gmail.com>
2025-04-12 14:53:37 +0530

    Create KthFactorOfN.java

---
+++ KthFactorOfN.java
@@ -0,0 +1,58 @@
+/**
+Problem :
+The kth Factor of n :
+You are given two positive integers n and k. A factor of an integer n is defined as an integer i where n % i == 0.
+
+Consider a list of all factors of n sorted in ascending order, return the kth factor in this list or return -1 if n has less than k factors.
+
+ 
+
+Example 1:
+
+Input: n = 12, k = 3
+Output: 3
+Explanation: Factors list is [1, 2, 3, 4, 6, 12], the 3rd factor is 3.
+Example 2:
+
+Input: n = 7, k = 2
+Output: 7
+Explanation: Factors list is [1, 7], the 2nd factor is 7.
+Example 3:
+
+Input: n = 4, k = 4
+Output: -1
+Explanation: Factors list is [1, 2, 4], there is only 3 factors. We should return -1.
+ 
+
+Constraints:
+
+1 <= k <= n <= 1000
+ 
+
+Follow up:
+
+Could you solve this problem in less than O(n) complexity?
+
+**/
+
+class Solution {
+    public int kthFactor(int n, int k) {
+        List<Integer> factors = new ArrayList<>();
+        int counter=0;
+        int inc = n%2 == 0 ? 1 : 2;
+
+        for(int i=1; i*i<=n; i+=inc) {
+            if(n%i == 0 ) {
+                counter++;
+                if(k==counter) return i;
+                if(i != n/i)
+                    factors.add(n/i);
+            }      
+        }
+
+        if(counter + factors.size() < k) return -1;
+
+        return factors.get(factors.size() - (k-counter));
+        
+    }
+}
